ca4163558ef6a374e4b98e7116c8776484691d2c
Lucie Nadvornik <73028743+lucienadvornik@users.noreply.github.com>
2025-05-29 23:26:13 +0200

    Update lambda_for_oura_data.py

--- lambda_for_oura_data.py
+++ lambda_for_oura_data.py
@@ -1,23 +1,29 @@
+#Libraries
 import json
 import boto3
 import requests
 from datetime import date
 
-# Nastavení
-API_TOKEN = "YOUR TOKEN"  
+# API configuration
+API_TOKEN = "UEJ5GU7KMOFISZHYQOPVLQOLFQOQ2VDR"  
 BASE_URL = "https://api.ouraring.com/v2/usercollection"
 HEADERS = {"Authorization": f"Bearer {API_TOKEN}"}
 
+# S3
 s3 = boto3.client("s3")
 BUCKET = "s3-for-oura-data"
+
+# Today's date
 TODAY = date.today().isoformat()
 
+# Fetch data from Oura API
 def fetch_oura(endpoint):
     url = f"{BASE_URL}/{endpoint}?start_date={TODAY}&end_date={TODAY}"
     response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
     return response.json()
 
+# Main script
 def lambda_handler(event, context):
     for data_type in ["sleep", "readiness", "activity"]:
         data = fetch_oura(f"daily_{data_type}")
